47ad7eae097c4e08aa17fb2ab39a6979179bc6bd
Daniel Kipkemei <daniel.kipkemei@andela.com>
2017-11-13 17:43:47 +0300

    [FEATURE 152749387] adding jquery for bootstrap and responsiveness on navbar

--- src/Components/Navhome.js
+++ src/Components/Navhome.js
@@ -7,15 +7,23 @@ class NavHome extends Component {
       <nav className="navbar navbar-inverse">
         <div className="container-fluid">
           <div className="navbar-header">
+            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
             <a className="b navbar-brand" href="/">Fancy shoppinglist</a>
           </div>
-          <ul className="nav navbar-nav">
-            <li ><a href="/" className="b">Home</a></li>
-          </ul>
-          <ul className="b2 nav navbar-nav navbar-right">
-            <li><a href="/register/" className="b"><span className="glyphicon glyphicon-user"></span> Sign Up</a></li>
-            <li><a href="/login/" className="b"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
-          </ul>
+          <div className="collapse navbar-collapse" id="myNavbar">
+            <ul className="nav navbar-nav">
+              <li ><a href="/" className="b"><span className="glyphicon glyphicon-home"></span> Home</a></li>
+            </ul>
+            <ul className="b2 nav navbar-nav navbar-right">
+              <li><a href="/register/" className="b"><span className="glyphicon glyphicon-user"></span> Sign Up</a></li>
+              <li><a href="/login/" className="b"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
+            </ul>
+
+          </div>
         </div>
       </nav>
       </div>
