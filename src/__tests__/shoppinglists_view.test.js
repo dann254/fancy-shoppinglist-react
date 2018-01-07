24b2cd6da5ba1631688970c54379a15ce5090227
Daniel Kipkemei <daniel.kipkemei@andela.com>
2018-01-07 17:50:57 +0300

    [CHORE 153863039] Adding tests for shoppinglist view

---
+++ src/__tests__/shoppinglists_view.test.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { shallow, mount, render } from "enzyme";
+import { Redirect } from "react-router-dom";
+import ReactDOM from "react-dom";
+import ShoppinglistView from "../Components/shoppinglist_view";
+import moxios from "moxios";
+import sinon from "sinon";
+import * as api from "../Components/API_URLS";
+
+let parentUrl;
+let SviewComponent;
+let prps = [{ name: "dan", id: 1, shared: true }];
+
+describe("sview component test cases", () => {
+  beforeEach(function() {
+    moxios.install();
+    parentUrl = {
+      url: api.shoppinglistsEP + 1
+    };
+    SviewComponent = shallow(
+      <ShoppinglistView match={parentUrl} shoppinglists={prps} />
+    );
+  });
+  afterEach(function() {
+    moxios.uninstall();
+  });
+  it("Renders Shoppinglists component", () => {
+    moxios.stubRequest(parentUrl.url, {
+      status: 200
+    });
+    expect(SviewComponent.length).toEqual(1);
+  });
+  it("Changes state when shoppinglist is edited", () => {
+    SviewComponent.instance().sendEditRequest("food", 1);
+    moxios.stubRequest(parentUrl.url, {
+      status: 200
+    });
+    moxios.wait(function() {
+      expect(ShoppinglistsComponent.instance().state.editSuccess).toBe(true);
+      done();
+    });
+  });
+  it("Changes state when shoppinglist deleted", () => {
+    SviewComponent.instance().deleteRequest();
+    moxios.stubRequest(parentUrl.url, {
+      status: 200
+    });
+    moxios.wait(function() {
+      expect(resendComponent.find("ToastContainer").text()).toContain(
+        "Shoppinglists deleted"
+      );
+      done();
+    });
+  });
+  it("Changes state when shoppinglist shared", () => {
+    SviewComponent.instance().shareRequest(1);
+    moxios.stubRequest(parentUrl.url, {
+      status: 200
+    });
+    moxios.wait(function() {
+      expect(resendComponent.find("ToastContainer").text()).toContain(
+        "Success"
+      );
+      done();
+    });
+  });
+  //
+  // it("calls componentWillMount", () => {
+  //   const testMountCall = sinon.spy(
+  //     Shoppinglists.prototype,
+  //     "componentWillMount"
+  //   );
+  //   const wrapper = shallow(<Shoppinglists />);
+  //   expect(testMountCall.calledOnce).toEqual(true);
+  // });
+});
