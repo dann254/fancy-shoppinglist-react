eff8a706634cc1c3979e97965cba68ece5a8c382
Daniel Kipkemei <daniel.kipkemei@andela.com>
2018-01-08 11:16:51 +0300

    [CHORE 153863039] update shoppinglits test

--- src/__tests__/Shoppinglist.test.js
+++ src/__tests__/Shoppinglist.test.js
@@ -27,8 +27,7 @@ describe("Buddies component test cases", () => {
     });
     expect(ShoppinglistsComponent.length).toEqual(1);
   });
-  it("Changes state when getBuddies is called", done => {
-    // const ShoppinglistsComponent = shallow(<ShoppingItemsPage match={parentUrl} />);
+  it("Changes state when getShoppinglit is called", done => {
     ShoppinglistsComponent.instance().getShoppinglists("");
     moxios.stubRequest(parentUrl.url, {
       status: 200,
@@ -42,13 +41,30 @@ describe("Buddies component test cases", () => {
       done();
     });
   });
-  //
-  // it("calls componentWillMount", () => {
-  //   const testMountCall = sinon.spy(
-  //     Shoppinglists.prototype,
-  //     "componentWillMount"
-  //   );
-  //   const wrapper = shallow(<Shoppinglists />);
-  //   expect(testMountCall.calledOnce).toEqual(true);
-  // });
+  it("Creates when addShoppinglit is called", () => {
+    ShoppinglistsComponent.instance().sendAddRequest("food");
+    moxios.stubRequest(parentUrl.url, {
+      status: 200,
+      response: {
+        name: "food",
+        shared: true
+      }
+    });
+    moxios.wait(function() {
+      expect(ShoppinglistsComponent.instance().state.addSuccess).toBe(true);
+      done();
+    });
+  });
+  it("Shows no shoppinglist when there are no shoppinglists", () => {
+    ShoppinglistsComponent.setState({
+      shoppinglists: [],
+      success: false,
+      message: "you dont have any shoppinglists with that name"
+    });
+    expect(ShoppinglistsComponent.find(".spanel-item-none")).toHaveLength(1);
+  });
+  it("Shows loading when request is not complete", () => {
+    ShoppinglistsComponent.setState({ sucess: false, shoppinglists: [] });
+    expect(ShoppinglistsComponent.find(".fa-spin")).toHaveLength(1);
+  });
 });
